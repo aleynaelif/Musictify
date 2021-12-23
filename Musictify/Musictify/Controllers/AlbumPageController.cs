17b969b9829fd1868b19f0d621db32b364623f81
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2021-12-23 23:24:36 +0300

    page design

---
+++ Musictify/Musictify/Controllers/AlbumPageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Musictify.Data;
+using Musictify.Models;
+
+namespace Musictify.Controllers
+{
+    public class AlbumPageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlbumPageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AlbumPage
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Album.Include(a => a.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+        
+        private bool AlbumExists(int id)
+        {
+            return _context.Album.Any(e => e.AlbumID == id);
+        }
+    }
+}
---
+++ Musictify/Musictify/Controllers/SingerPageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Musictify.Data;
+using Musictify.Models;
+
+namespace Musictify.Controllers
+{
+    public class SingerPageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SingerPageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SingerPage
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Singer.ToListAsync());
+        }
+
+        private bool SingerExists(int id)
+        {
+            return _context.Singer.Any(e => e.SingerID == id);
+        }
+    }
+}
